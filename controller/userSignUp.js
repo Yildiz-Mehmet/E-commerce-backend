30328534b40bb347c4577fd6097f4926973fa09a
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-10 18:28:22 +0300

    ok

--- controller/userSignUp.js
+++ controller/userSignUp.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/userModel")
+const bcrypt = require('bcryptjs');
+
 
 async function userSignUpController(req,res){
     try{
@@ -14,8 +16,26 @@ async function userSignUpController(req,res){
         if(!name){
             throw new Error("Please provide name")
         }
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = await bcrypt.hashSync(password, salt);
 
-        const userData = new userModel(req.body)
+        if(!hashPassword){
+            throw new Error("Something is wrong")
+        }
+        const payload ={
+            ...req.body,
+            password : hashPassword
+        }
+
+        const userData = new userModel(payload)
+        const saveUser = userData.save()
+
+        res.status(201).json({
+            data:saveUser,
+            success:true,
+            error:false,
+            message:"User created Successfully!"
+        })
     }
     catch(err) {
         res.json({
@@ -27,3 +47,6 @@ async function userSignUpController(req,res){
         })
     }
 }
+
+
+module.exports = userSignUpController

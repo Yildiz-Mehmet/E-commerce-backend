6d38bf8195413233a7b3856ae02904c8529658fc
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-10 19:03:43 +0300

    ok

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 app.use("/api",router)
 
-const PORT = 8080 || process.env.PORT 
+const PORT = 8080 || process.env.MONGODB_URI
 
 
 connectDB().then(()=>{

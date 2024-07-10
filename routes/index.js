a2efbe5a0cceb4bc05605ba23545fccc8d869980
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-10 23:41:59 +0300

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
 const router = express.Router()
-
 const userSignUpController = require("../controller/userSignUp")
+
+
+
 
 router.post("/signup",userSignUpController)
 

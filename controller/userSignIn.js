84cdaa1bf2a5ca61159dd3f054c64216049848f6
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-12 11:54:19 +0300

    ok

--- controller/userSignIn.js
+++ controller/userSignIn.js
@@ -1,6 +1,21 @@
 async function userSignInController(req,res){
     try{
 
+        const {email,password} = req.body
+
+        if(!email){
+            throw new Error("Please provide email")
+        }
+        if(!password){
+            throw new Error("Please provide password")
+        }
+
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            throw new Error("User not found")
+        }
+
     }
     catch(err){
         res.json({

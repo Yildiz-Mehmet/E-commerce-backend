f5c20ee5f17ccae4c5ac76bfaeb2323646e37133
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-12 16:39:28 +0300

    ok

--- controller/userSignIn.js
+++ controller/userSignIn.js
@@ -25,6 +25,27 @@ async function userSignInController(req,res){
 
         if(checkPassword){
 
+    const tokenData ={
+        _id : user._id,
+        email : user.email,
+
+    }
+
+       const token= await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY  , { expiresIn: 60 * 60 *8 });
+
+       
+       const tokenOption = {
+        httpOnly : true,
+        secure : true
+       }
+       
+       res.cookie("token",token,tokenOption).json({
+        message : "Login successfully",
+        data : token,
+        success : true,
+        error : false
+       })
+
         }else{
             throw new Error("Please check password")
         }
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
+const cookieParser = require('cookie-parser')
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/api",router)
+app.use(cookieParser())
 
 const PORT = 8080 || process.env.MONGODB_URI
 

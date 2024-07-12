232f6ca9533a315cddf37eb9166d87e24cced16c
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-12 17:38:14 +0300

    ok

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const cookieParser = require('cookie-parser')
 
 
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        origin : process.env.FRONTEND_URL,
+        credentials : true
+    }
+))
 app.use(express.json())
 app.use("/api",router)
 app.use(cookieParser())

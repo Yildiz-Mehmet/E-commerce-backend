0801b99ceecdb6b95a0b9b98db79a1526cdfd49d
Yildiz-Mehmet <mesel4461@gmail.com>
2024-07-13 11:57:05 +0300

    ok

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,7 +1,18 @@
+const jwt = require('jsonwebtoken');
+
 async function authToken(req,res,next){
     try {
         
         const token = req.cookies?.token || req.header
+
+        console.log("token" , token)
+/* 
+          jwt.verify(token, process.env.TOKEN_SECRET_KEY, function(err, decoded) {
+            console.log(err)
+            console.log("decoded",decoded)
+            
+ 
+        }); */
     
     } catch (err) {
         res.status(400).json({
